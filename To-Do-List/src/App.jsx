f39954444e15ca5bee0b4fb764bb65f860b0608f
Dhruv Divecha <157246271+dhruvdivecha@users.noreply.github.com>
2025-01-03 15:00:29 +0300

    first commit

---
+++ To-Do-List/src/App.jsx
@@ -0,0 +1,16 @@
+import ToDoCard from "./components/ToDoCard"
+import ToDoList from "./components/ToDoList"
+import ToDoInput from "./components/ToDoInput"
+
+function App() {
+  
+  return (
+     <main>
+        <ToDoInput />
+        <ToDoList />
+        <ToDoCard />
+     </main>
+  )
+}
+
+export default App
